6fba4c0ce1e0508fb5ba4e5c313f330262b22e9d
Raghuram <raghu17122001@gmail.com>
2022-10-31 20:40:05 +0530

    add aws db

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'guvi');
-define('DB_PASSWORD', 'guvi123');
-define('DB_NAME', 'guvi');
+define('DB_SERVER', 'database-guvi-raghu.ck2rgsp30sfg.ap-south-1.rds.amazonaws.com');
+define('DB_USERNAME', 'guvi_user');
+define('DB_PASSWORD', 'guvipass');
+define('DB_NAME', 'guvi_database');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
